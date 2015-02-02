7385b3eb6ca82b660a00d5fab85787df9b041eb8
tyleri <tyler.ishikawa@outlook.com>
2015-02-02 08:12:16 -0500

    adds homeroom periods

--- js/scripts.js
+++ js/scripts.js
@@ -49,54 +49,53 @@ Date.prototype.toString = function() {
 };
 
 var regSchedule = [
-	{start: new Time(8), end: new Time(15,35)},
-	{start: new Time(8), end: new Time(8,41)},
-	{start: new Time(8,45), end: new Time(9,26)},
-	{start: new Time(9,31), end: new Time(10,15)},
-	{start: new Time(10,20), end: new Time(11,1)},
-	{start: new Time(11,6), end: new Time(11,47)},
-	{start: new Time(11,52), end: new Time(12,33)},
-	{start: new Time(12,38), end: new Time(13,19)},
-	{start: new Time(13,24), end: new Time(14,5)},
-	{start: new Time(14,9), end: new Time(14,50)},
-	{start: new Time(14,54), end: new Time(15,35)}
+{name: "Period 1", id: "Period1", start: new Time(8), end: new Time(8,41)},
+{name: "Period 2", id: "Period2", start: new Time(8,45), end: new Time(9,26)},
+{name: "Period 3", id: "Period3", start: new Time(9,31), end: new Time(10,15)},
+{name: "Period 4", id: "Period4", start: new Time(10,20), end: new Time(11,1)},
+{name: "Period 5", id: "Period5", start: new Time(11,6), end: new Time(11,47)},
+{name: "Period 6", id: "Period6", start: new Time(11,52), end: new Time(12,33)},
+{name: "Period 7", id: "Period7", start: new Time(12,38), end: new Time(13,19)},
+{name: "Period 8", id: "Period8", start: new Time(13,24), end: new Time(14,5)},
+{name: "Period 9", id: "Period9", start: new Time(14,9), end: new Time(14,50)},
+{name: "Period 10", id: "Period10", start: new Time(14,54), end: new Time(15,35)}
 ];
 
 var hrSchedule = [
-	{start: new Time(8), end: new Time(15,35)},
-	{start: new Time(8), end: new Time(8,40)},
-	{start: new Time(8,45), end: new Time(9,25)},
-	{start: new Time(9,29), end: new Time(10,9)},
-	{start: new Time(10,30), end: new Time(11,10)},
-	{start: new Time(11,14), end: new Time(11,54)},
-	{start: new Time(11,58), end: new Time(12,38)},
-	{start: new Time(12,42), end: new Time(13,22)},
-	{start: new Time(13,26), end: new Time(14,6)},
-	{start: new Time(14,10), end: new Time(14,50)},
-	{start: new Time(14,55), end: new Time(15,35)}
+{name: "Period 1", id: "Period1", start: new Time(8), end: new Time(8,40)},
+{name: "Period 2", id: "Period2", start: new Time(8,45), end: new Time(9,25)},
+{name: "Period 3", id: "Period3", start: new Time(9,29), end: new Time(10,9)},
+{name: "Homeroom", id: "HR", start: new Time(10,13), end: new Time(10,25)},
+{name: "Period 4", id: "Period4", start: new Time(10,30), end: new Time(11,10)},
+{name: "Period 5", id: "Period5", start: new Time(11,14), end: new Time(11,54)},
+{name: "Period 6", id: "Period6", start: new Time(11,58), end: new Time(12,38)},
+{name: "Period 7", id: "Period7", start: new Time(12,42), end: new Time(13,22)},
+{name: "Period 8", id: "Period8", start: new Time(13,26), end: new Time(14,6)},
+{name: "Period 9", id: "Period9", start: new Time(14,10), end: new Time(14,50)},
+{name: "Period 10", id: "Period10", start: new Time(14,55), end: new Time(15,35)}
 ];
 
 var AMCHalfDay = [
-	{start: new Time(8,35), end: new Time(12,41)},
-	{start: new Time(8,35), end: new Time(8,56)},
-	{start: new Time(9), end: new Time(9,21)},
-	{start: new Time(9,25), end: new Time(9,46)},
-	{start: new Time(9,50), end: new Time(10,11)},
-	{start: new Time(10,15), end: new Time(10,36)},
-	{start: new Time(10,40), end: new Time(11,1)},
-	{start: new Time(11,5), end: new Time(11,26)},
-	{start: new Time(11,30), end: new Time(11,51)},
-	{start: new Time(11,55), end: new Time(12,16)},
-	{start: new Time(12,20), end: new Time(12,41)}
+{name: "Homeroom", id: "FirstHR", start: new Time(8,10), end: new Time(8,30)},
+{name: "Period 1", id: "Period1", start: new Time(8,35), end: new Time(8,56)},
+{name: "Period 2", id: "Period2", start: new Time(9), end: new Time(9,21)},
+{name: "Period 3", id: "Period3", start: new Time(9,25), end: new Time(9,46)},
+{name: "Period 4", id: "Period4", start: new Time(9,50), end: new Time(10,11)},
+{name: "Period 5", id: "Period5", start: new Time(10,15), end: new Time(10,36)},
+{name: "Period 6", id: "Period6", start: new Time(10,40), end: new Time(11,1)},
+{name: "Period 7", id: "Period7", start: new Time(11,5), end: new Time(11,26)},
+{name: "Period 8", id: "Period8", start: new Time(11,30), end: new Time(11,51)},
+{name: "Period 9", id: "Period9", start: new Time(11,55), end: new Time(12,16)},
+{name: "Period 10", id: "Period10", start: new Time(12,20), end: new Time(12,41)}
 ];
 
 
 
 function updateSchedule(schedule) {
-	for (var i = 1; i < schedule.length; i++) {
+	for (var i = 0; i < schedule.length; i++) {
 		var str = schedule[i].start.toString() + " - " +
 			schedule[i].end.toString();
-		$("#Period" + i + ">.time").html(str);
+		$("#" + schedule[i].id + ">.time").html(str);
 	}
 }
 
@@ -112,24 +111,24 @@ function updatePeriod(schedule) {
 	var dow = today.getDay(); // day of week
 	var currTime = new Time(today.getHours(), today.getMinutes());
 
-	if ( ! currTime.isIn(schedule[0].start, schedule[0].end) ||
+	if ( ! currTime.isIn(schedule[0].start, schedule[schedule.length-1].end) ||
 			dow == 6 || dow == 0) {
 		// If not during school
-		for (var i = 1; i < schedule.length; i++)
-				if ( $("#Period" + i).hasClass("info") )
+		for (var i = 0; i < schedule.length; i++)
+				if ( $("#" + schedule[i].id).hasClass("info") )
 					$("#Period" + i).removeClass("info");
 
 		if ( $("#per").html() != "" || $("#min-left").html() != "" ) {
 			$("#per").html("");
 			$("#min-left").html("");
 		}
-		var minutes = schedule[0].start.subtract( currTime );
+		var minutes = schedule[0].start - currTime;
 		var hours = Math.floor(minutes / 60);
 		minutes %= 60;
 
 		// add 24 or 48 hours if on the weekend
 		hours +=
-			(dow == 5 && currTime > schedule[0].end) ||
+			(dow == 5 && currTime > schedule[schedule.length-1].end) ||
 			(dow == 6 && currTime < schedule[0].start) ?
 			24 * 2 :
 			(dow == 6 && currTime > schedule[0].start) ||
@@ -142,21 +141,22 @@ function updatePeriod(schedule) {
 		// Updates current period if during school
 		if ( $("#until-school").html() != "" )
 			$("#until-school").html("");
-		var min;
-		for (var i = 1; i < schedule.length; i++) {
-			if ( currTime.isIn(schedule[i].start,schedule[i].end) ) {
-				$("#per").html("Period " + i);
-				if ( ! $("#Period" + i).hasClass("info") )
-					$("#Period" + i).addClass("info");
-				min = schedule[i].end.subtract(currTime);
+		var min, curr;
+		for (var i = 0; i < schedule.length; i++) {
+			curr = schedule[i];
+			if ( currTime.isIn(curr.start, curr.end) ) {
+				$("#per").html(curr.name);
+				if ( ! $("#" + curr.id).hasClass("info") )
+					$("#" + curr.id).addClass("info");
+				min = curr.end - currTime;
 				$("#min-left").html( min + " minutes left");
-			} else {
-				if ( $("#Period" + i).hasClass("info") )
-					$("#Period" + i).removeClass("info");
+			} else if (i != 0) {
+				if ( $("#" + curr.id).hasClass("info") )
+					$("#" + curr.id).removeClass("info");
 
-				if ( currTime.isIn(schedule[i-1].end,schedule[i].start) ) {
-					$("#per").html("Before Period " + i);
-					min = schedule[i].start.subtract(currTime);
+				if ( currTime.isIn(schedule[i-1].end,curr.start) ) {
+					$("#per").html("Before " + curr.name);
+					min = curr.start - currTime;
 					$("#min-left").html( min + " minutes left");
 				} 
 			}
@@ -171,6 +171,8 @@ function setBackground() {
 $('#schedule a[href="#regular"]').click(function (e) {
 	e.preventDefault();
 	$(this).tab('show');
+	$('#HR').hide();
+	$('#FirstHR').hide();
 	updateSchedule(regSchedule);
 	updatePeriod(regSchedule);
 	schedule = regSchedule;
@@ -178,6 +180,8 @@ $('#schedule a[href="#regular"]').click(function (e) {
 $('#schedule a[href="#homeroom"]').click(function (e) {
 	e.preventDefault();
 	$(this).tab('show');
+	$('#HR').show();
+	$('#FirstHR').hide();
 	updateSchedule(hrSchedule);
 	updatePeriod(hrSchedule);
 	schedule = hrSchedule;
@@ -185,6 +189,8 @@ $('#schedule a[href="#homeroom"]').click(function (e) {
 $('#schedule a[href="#AMChalfday"]').click(function (e) {
 	e.preventDefault();
 	$(this).tab('show');
+	$('#HR').hide();
+	$('#FirstHR').show();
 	updateSchedule(AMCHalfDay);
 	updatePeriod(AMCHalfDay);
 	schedule = AMCHalfDay;
